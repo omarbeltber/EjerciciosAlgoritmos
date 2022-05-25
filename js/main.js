04356c8cc05b0973d4a3c7aad9b525eb46ce91b0
omarbeltber <omarbeltber@gmail.com>
2022-05-25 16:44:10 -0500

    Primer commit de los ejercicios de Javascript

---
+++ js/main.js
@@ -0,0 +1,106 @@
+//1. Solicitar 3 números (entre el 1 y el 100) y definir cual es el menor
+
+
+/*
+let x1 = prompt("Pon el primer número");
+if(x1< 1 || x1 >100){
+    alert("Pon nuevamente");
+    let x1 = prompt("Pon el primer número");
+}
+let x2 = prompt("Pon el segundo número");
+if(x2< 1 || x2 >100){
+    alert("Pon nuevamente");
+    let x2 = prompt("Pon el segundo número");
+}
+let x3 = prompt("Pon el tercer número");
+if(x3< 1 || x3 >100){
+    alert("Pon nuevamente");
+    let x3 = prompt("Pon el tercer número");
+}
+
+if((x1 < x2) && (x1 < x3)){
+    alert("El número más chico es" + x1);
+}
+else if((x2 < x1) && (x2 < x3)){
+    alert("El número más chico es" + x2);
+}
+else{
+    alert("El número más chico es" + x3);
+}
+
+//2. Solicitar 3 números (entre el 1 y el 100) y definir cual es el mayor
+let y1 = prompt("Pon el primer número");
+if(y1< 1 || y1 >100){
+    alert("Pon nuevamente");
+    let y1 = prompt("Pon el primer número");
+}
+let y2 = prompt("Pon el segundo número");
+if(y2< 1 || y2 >100){
+    alert("Pon nuevamente");
+    let y2 = prompt("Pon el segundo número");
+}
+let y3 = prompt("Pon el tercer número");
+if(y3< 1 || y3 >100){
+    alert("Pon nuevamente");
+    let y3 = prompt("Pon el tercer número");
+}
+
+if((y1 > y2) && (y1 > y3)){
+    alert("El número más grande es" + y1);
+}
+else if((y2 > y1) && (y2 > y3)){
+    alert("El número más grande es" + y2);
+}
+else{
+    alert("El número más grande es" + y3);
+} 
+*/
+//3. Suspendido
+//4.Solicitar un entero (entre 100 y 200) y determinar si es múltiplo de 3
+
+/*let z1 = prompt("Pon un número entre 100 y 200");
+if(z1< 100 || z1 >200){
+    alert("Intenta nuevamente");
+    let z1 = prompt("Ingresa un número entero");
+}
+if((z1 % 3) == 0){
+    alert("Si es multiplo de 3, Su Número " + z1);
+}
+
+else{
+    alert("No es multiplo de 3, Su Número " + z1);
+}*/
+
+//5.Elaborar un algoritmo para leer 3 números y determinar si uno es la suma de los otros dos
+
+/*let x1 = prompt("Ingresa el primer número");
+let x2 = prompt("Ingresa el segundo número");
+let x3 = prompt("Ingresa el tercer número");
+
+x1=parseFloat(x1,10)
+x2=parseFloat(x2,10)
+x3=parseFloat(x3,10)
+
+if((x1 + x2) == x3) {
+    alert( x3 + " Es la suma de los otros dos números");
+}
+else if((x1 + x3) == x2) {
+    alert(x2 + " Es la suma de los otros dos números");
+}
+else if((x2 + x3) == x1) {
+    alert(x1 + " Es la suma de los otros dos números");
+}
+else{
+    alert("La suma de dos números no dan uno de nuestros números");
+}*/
+//6.Elabora un algoritmo para determinar si es par o impar
+
+/*let x = prompt("Ingresa cualquier número");
+x = parseFloat(x,10)
+
+if(x % 2 == 0) {
+    alert(x + " Su número es par");
+}
+else{
+    alert(x + " Su número es impar");
+}*/
